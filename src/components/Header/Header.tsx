a6234f136cd2e9dacd9c78b974accc908342d888
Maksim Bekis <maxamax997@gmail.com>
2024-03-17 01:43:19 +0300

    fix burger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,25 @@ import "./../../style/scss/allStyle.scss";
 import { useAddBasket } from "../../hooks/useAddBasket";
 import { NavLink } from "react-router-dom";
 import { Basket } from "../Basket/Basket";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Header: React.FC = () => {
   const [toggleBasket, setToggleBasket] = useState(false);
-  const [toggleBurger, setToggleBurger] = useState(false);
+  const [toggleBurger, setToggleBurger] = useState(true);
 
+  // useEffect(() => {
+  //   //   window.addEventListener("resize", () => setWidth(window.innerWidth));
+  //   //   setWidth(window.innerWidth);
+  //   if (window.innerWidth > 480) {
+  //     // console.log(width);
+  //     setToggleBurger(false);
+  //   }
+  //   if (window.innerWidth <= 480) {
+  //     // console.log(width);
+  //     setToggleBurger(true);
+  //   }
+  //   console.log(window.innerWidth);
+  // }, []);
   const { basket } = useAddBasket();
   return (
     <header className={st.header}>
@@ -21,7 +34,7 @@ export const Header: React.FC = () => {
         {/* <nav className={st.header__navigating}> */}
         <nav
           className={
-            toggleBurger ? st.header__navigating__burger : st.header__navigating
+            st.header__navigating
           }
         >
           <ul>
@@ -32,7 +45,7 @@ export const Header: React.FC = () => {
               <NavLink to="orders">Заказы</NavLink>
             </li>
             <li>
-              <NavLink to="basket">Корзина</NavLink>
+              <NavLink to="basket">Корзина ({basket.length})</NavLink>
             </li>
           </ul>
         </nav>
@@ -40,15 +53,18 @@ export const Header: React.FC = () => {
           onClick={() => setToggleBurger(!toggleBurger)}
           className={st.burger}
         >
-          +++
+          <span></span>
+          <span></span>
+          <span></span>
+       
         </div>
-        {/* <div
+        <div
           onClick={() => setToggleBasket(!toggleBasket)}
           className={st.header__cart}
         >
           <img src={cart} alt="Cart" />
           <p>Корзина({basket.length})</p>
-        </div> */}
+        </div>
         <div
           className={st.containerBasket}
           style={{
